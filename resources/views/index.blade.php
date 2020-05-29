5c6284d71c79bfd011703cb749d9e0f9f04bbe3b
rrabina <rrabina4@gmail.com>
2020-05-29 12:53:03 +0545

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/app')
+
+@section('content')
+    <div class="container">
+        <center></br><h3 class="display-4">Budget Bytes</h3></center>
+
+       <p> Budget Bytes is a food blog that aims to provide delicious recipes designed for small budgets. It recognizes that we can’t all afford to use expensive ingredients and cook recipes that take hours to prepare. It tries to cater to those with “Instagram taste and a peanut butter budget.”
+       </p>
+       <p>
+       This food blog has developed six principles to keep the grocery budget low and reduce waste:
+       </p>
+1. Plan your meals</br>
+2. Use ingredients wisely</br>
+3. Portion control</br>
+4. Don't be afraid of leftovers</br>
+5. The freezer is your friend</br>
+6. Shop wisely</br>
+    </p>
+    <p>
+    </p>
+    
+   <p> Enjoy this blog!</p>
+</div>
+@endsection
